0e7e29b3e5cf3fc1cf5ed9b39a15800ba16e9acf
BMWSawyer <bradley.mw.sawyer@gmail.com>
2021-07-26 19:18:49 +0000

    Updated and fixed inaccurate code

--- without.js
+++ without.js
@@ -1,15 +1,37 @@
-const testWords = ["hello", "world", "lighthouse"];
-const testNums = [1, 2, 3];
-const testNumString = ["1", "2", "3"];
+const eqArrays = function(Arr1, Arr2) {
+  if (Arr1.length === Arr2.length){
+    
+    for (let i = 0; i < Arr1.length; i++) {
+      if (Arr1[i] !== Arr2[i]){
+        return false;
+      }
+    };
+
+    return true;
+
+  } else {
+    return false;
+  }
+}
+
+const assertArraysEqual = function(Arr1, Arr2) {
+  if (eqArrays(Arr1, Arr2) === true) {
+    console.log(`✅✅ Assertion Paased: ${Arr1} === ${Arr2}`)
+  } else {
+    console.log(`❌❌ Assertion Failed: ${Arr1} !== ${Arr2}`)
+  }
+}
+
 
 const without = function(Arr1, Arr2) {
   let newArray = [];
-  let arrayStatus = true;
+  let arrayStatus;
+  
   for (let i = 0; i < Arr1.length; i++) {
+    arrayStatus = true;
+    
     for (let x = 0; x < Arr2.length; x++) {
-      if (Arr1[i] !== Arr2[x]){
-        arrayStatus = true;
-      } else {
+      if (Arr1[i] === Arr2[x]){
         arrayStatus = false;
       }
     };
@@ -19,35 +41,18 @@ const without = function(Arr1, Arr2) {
     }
   };
 
-  console.log(newArray);
+  return newArray;
 }
 
-const assertArraysEqual = function(Arr1, Arr2) {
-  
-  if (Arr1.length === Arr2.length){
-    let matchResult = true;
-    for (let i = 0; i < Arr1.length; i++) {
-      if (Arr1[i] === Arr2[i]){
-        matchResult = true;
-      } else {
-        matchResult = false;
-      }
-    };
+const testWords = ["hello", "world", "lighthouse"];
+const testNums = [1, 2, 3];
+const testNumString = ["1", "2", "3"];
 
-    if (matchResult === true) {
-      console.log(`✅✅ Assertion Paased: ${Arr1} === ${Arr2}`)
-    } else {
-      console.log(`❌❌ Assertion Failed: ${Arr1} !== ${Arr2}`)
-    }
-  
-  } else {
-    console.log(`❌❌ Assertion Failed: ${Arr1} !== ${Arr2}`);  
-  }
-}
+let result1 = without(testWords, ["lighthouse"]);
+let result2 = without(testNums, [1]);
+let result3 = without(testNumString, [1, 2, "3"]);
 
-without(testWords, ["lighthouse"]);
-without(testNums, [1]);
-without(testNumString, [1, 2, "3"]);
+assertArraysEqual(result1, ["hello", "world"]);
 
 assertArraysEqual(testWords, ["hello", "world", "lighthouse"]);
 assertArraysEqual(testNums, [1, 2, 3]);
