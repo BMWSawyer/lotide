e72501a1a3844274e1d7906dc91b69dc20fa007c
BMWSawyer <bradley.mw.sawyer@gmail.com>
2021-07-26 04:20:26 +0000

    Updated the name of the function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,4 +1,4 @@
-const eqArrays = function(Arr1, Arr2) {
+const assertArraysEqual = function(Arr1, Arr2) {
   
   if (Arr1.length === Arr2.length){
     let matchResult = true;
@@ -21,8 +21,8 @@ const eqArrays = function(Arr1, Arr2) {
   }
 }
 
-eqArrays([1, 2, 3], [1, 2, 3]);
-eqArrays([1, 2, 3], [1, 2, 3, 4]);
-eqArrays([1, 2, 3], [3, 2, 1]);
-eqArrays(["4", "5", "6"], ["4", "5", "6"]);
-eqArrays(["4", "5", "6"], ["4", "5", 6]);
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual(["4", "5", "6"], ["4", "5", "6"]);
+assertArraysEqual(["4", "5", "6"], ["4", "5", 6]);
