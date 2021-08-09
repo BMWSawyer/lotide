bff571e54adde6621fab9b1e7d5783e1de895e57
BMWSawyer <bradley.mw.sawyer@gmail.com>
2021-08-09 20:30:29 +0000

    Refactored the code in this file to include module so that we can access the code from other files and refactor them as well.

--- assertEqual.js
+++ assertEqual.js
@@ -6,7 +6,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Lighthouse Labs", "Lighthouse Labs");
-assertEqual(50, 100);
+module.exports = assertEqual;
