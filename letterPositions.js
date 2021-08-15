307789d0a121c802a7e29027a3b92d270a4a7660
BMWSawyer <bradley.mw.sawyer@gmail.com>
2021-08-15 01:12:57 +0000

    Updated the code to require the previously copied in eqArrays and assertArraysEqual functions as well as export the letterPositions function

--- letterPositions.js
+++ letterPositions.js
@@ -1,3 +1,4 @@
+/*
 const eqArrays = function(Arr1, Arr2) {
   if (Arr1.length === Arr2.length){
     
@@ -21,6 +22,9 @@ const assertArraysEqual = function(Arr1, Arr2) {
     console.log(`❌❌ Assertion Failed: ${Arr1} !== ${Arr2}`)
   }
 }
+*/
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
 
 const letterPositions = function(sentence) {
@@ -42,6 +46,8 @@ const letterPositions = function(sentence) {
   }
   return results;
 };
+
+module.exports = letterPositions;
 
 test1 = letterPositions("hello").e;
 
