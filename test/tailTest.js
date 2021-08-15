df74bf3cdbe9a79d8341ed363b5a653d2b42bc58
BMWSawyer <bradley.mw.sawyer@gmail.com>
2021-08-15 00:32:16 +0000

    Updated the code to use mocha and chai for testing. Used the previous test that I had but also added in two other tests as well

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,25 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
+//const assertEqual = require('../assertEqual');
 
+/*
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 
 console.log(tail(words));
 assertEqual(words.length, 3);
+*/
+
+describe("#tail", () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Yo Yo', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
+  });
+
+  it("returns [4, 6, 8, 10, 12] for [2, 4, 6, 8, 10, 12]", () => {
+    assert.deepEqual(tail([2, 4, 6, 8, 10, 12]), [4, 6, 8, 10, 12]);
+  });
+
+  it("returns [] for [1]", () => {
+    assert.deepEqual(tail([1]), []);
+  });
+
+});
