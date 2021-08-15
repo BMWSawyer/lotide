b388e377e12f289a7a312d197bdd10c4090a0b75
BMWSawyer <bradley.mw.sawyer@gmail.com>
2021-08-15 00:25:46 +0000

    Updated the code to use mocha and chai for testing instead of the assertArraysEqual function. Created new tests for each of the preivous tests I had

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,8 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+//const assertArraysEqual = require('../assertArraysEqual');
 
-
+/*
 let result1 = middle([1, 2, 3, 4, 5]);
 let result2 = middle([2, 4, 6, 8, 10, 12]);
 let result3 = middle([1, 3, 5, 7, 9, 11, 13]);
@@ -19,3 +20,26 @@ assertArraysEqual(result2, expectedOutput2);
 assertArraysEqual(result3, expectedOutput3);
 assertArraysEqual(result4, expectedOutput4);
 assertArraysEqual(result5, expectedOutput5);
+*/
+
+describe("#middle", () => {
+  it("returns [3] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+
+  it("returns [6, 8] for [2, 4, 6, 8, 10, 12]", () => {
+    assert.deepEqual(middle([2, 4, 6, 8, 10, 12]), [6, 8]);
+  });
+
+  it("returns [7] for [1, 3, 5, 7, 9, 11, 13]", () => {
+    assert.deepEqual(middle([1, 3, 5, 7, 9, 11, 13]), [7]);
+  });
+
+  it("returns [] for [1]", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+
+  it("returns [] for [1, 2]", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+});
